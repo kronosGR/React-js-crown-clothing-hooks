3dcfe4af03456f7377958b0129e33f8920d02dde
KronosGR <geo.elgeo@gmail.com>
2021-10-31 13:41:14 +0100

    changed the ...

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,15 +1,15 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: 'AIzaSyATNRIHX7Alx6C9QgxYTUa4IhbMWVkiSag',
+  authDomain: 'crown-db-74ea1.firebaseapp.com',
+  projectId: 'crown-db-74ea1',
+  storageBucket: 'crown-db-74ea1.appspot.com',
+  messagingSenderId: '131312504931',
+  appId: '1:131312504931:web:d4e706ad9b6b108134e856',
+  measurementId: 'G-ZR9K68016Y',
 };
 
 firebase.initializeApp(config);
